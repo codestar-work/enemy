a12cd0394bbba8fa008cb78c769a66f8027af147
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2016-06-13 19:34:40 +0700

    Post data using Multer

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var express = require('express')
 var app = express()
+var multer = require('multer')
+var upload = multer({dest: 'uploads/'})
+
 app.use(express.static('public'))
 app.engine('html', require('ejs').renderFile)
 app.get('/', home)
-app.get('/register', register)
+app.get ('/register', register)
+app.post('/register', upload.array(), registerNewUser)
 app.listen(8000)
 
 function home(req, res) {
@@ -12,4 +16,9 @@ function home(req, res) {
 
 function register(req, res) {
 	res.render('register.html')
+}
+
+function registerNewUser(req, res) {
+	console.log(req.body)
+	res.redirect('/')
 }
