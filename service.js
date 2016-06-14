c6b42993fdc2d522f9827528aa7ff1d20b585243
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2016-06-14 20:35:41 +0700

    Add new service

--- service.js
+++ service.js
@@ -5,11 +5,23 @@ var mongoUrl = "mongodb://127.0.0.1/airport"
 app.get('/', sendStatus)
 app.get('/list', list)
 app.get('/search', search)
+app.get('/search-by-country', searchByCountry)
 app.listen(8100)
+
+function searchByCountry(req, res) {
+    // req.query.name
+    var pattern = new RegExp(req.query.name, "i")
+    mongo.connect(mongoUrl,
+        (error, db) => db.collection('airport')
+        .find({country: pattern}).toArray(
+            (error, data) => res.send(data)
+        )
+    )
+}
 
 function search(req, res) {
     // req.query.name -> su
-    var pattern = new RegExp(req.query.name)
+    var pattern = new RegExp(req.query.name, "i")
     mongo.connect(mongoUrl, 
         (error, db) => db.collection('airport')
         .find({name: pattern}).toArray(
